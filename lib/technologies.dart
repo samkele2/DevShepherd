6a628a74c05b4c42d4b8d3c4a16bdd9246212a9f
Samkele Songo Sonwabiso Njiva <sanjicpt025@student.wethinkcode.co.za>
2026-09-23 15:59:42 +0200

    technologies added to DevShepherd

---
+++ lib/technologies.dart
@@ -0,0 +1,6 @@
+import 'package:dev_shepherd/skill.dart';
+import 'package:dev_shepherd/technology.dart';
+
+class Technologies {
+  List<Technology> technologies = [Technology("Dart", [Skill("Variable"), Skill("Strings"), Skill("Loops")])];
+}
